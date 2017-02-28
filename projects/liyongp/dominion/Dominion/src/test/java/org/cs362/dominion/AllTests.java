6cdfc9f392d2976d990a969f49f297104b111133
YongPing Li <liyongp@oregonstate.edu>
2017-02-27 17:26:53 -0800

    Added Evosuite&CoverageHTML&RandomTester

--- projects/liyongp/dominion/Dominion/src/test/java/org/cs362/dominion/AllTests.java
+++ projects/liyongp/dominion/Dominion/src/test/java/org/cs362/dominion/AllTests.java
@@ -1,9 +1,11 @@
+package main.java.org.cs362.dominion;
+
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ CardTest.class, Player_GameState_Test.class })
+@SuiteClasses({ Card_ESTest.class, GameState_ESTest.class, PlayDominion_ESTest.class, Player_ESTest.class, RandomTesterDominion_ESTest.class })
 public class AllTests {
 
 }
