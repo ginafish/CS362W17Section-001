fab7bc4325c9cd3e7889ecbfb273077bd6500f08
Arman Hastings <armanhastings@gmail.com>
2017-03-14 01:25:04 -0700

    Add files via upload

---
+++ projects/hastinar/ass4/dominion/src/main/java/org/cs325/dominion/App.java
@@ -0,0 +1,13 @@
+package org.cs325.dominion;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
