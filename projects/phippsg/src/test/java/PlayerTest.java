20893759f441e38321560ef8278d8ebf880fa2e8
ginafish <phippsg@oregonstate.edu>
2017-03-20 11:30:43 -0700

    Pitest isn't running tests
    Regenerating evosuite tests since it looks like evosuite is what's breaking it.

--- projects/phippsg/src/test/java/PlayerTest.java
+++ projects/phippsg/src/test/java/PlayerTest.java
@@ -7,26 +7,47 @@ import org.junit.Test;
 public class PlayerTest {  
   
   @Test  
-  public void testConstructor() {  
-    Player testPlayer = new Player();  
-    assertEquals(0, testPlayer.getHandSize());  
-  }  
+  public void testConstructor() {
+    Player testPlayer = new Player();
+    assertEquals(0, testPlayer.getHandSize());
+  }
     
-  @Test  
-  public void testGain() {  
-    Player testPlayer = new Player();  
+  @Test
+  public void testGain() {
+    Player testPlayer = new Player();
     testPlayer.gain(new Card("Copper"));  
     assertEquals(0, testPlayer.getHandSize());  
     testPlayer.gainToHand(new Card("Silver"));  
     assertEquals(1, testPlayer.getHandSize());  
-  }  
+  }
     
   @Test  
   public void testInitialize() {  
-    Player testPlayer = new Player();  
+    Player testPlayer = new Player(); 
+    assertEquals(0, testPlayer.getHandSize());
     testPlayer.initilizeDeck();  
     assertEquals(5, testPlayer.getHandSize());  
-  }  
+  }
   
+	@Test
+	public void testHandPlay() {
+		Player testPlayer = new Player();
+		testPlayer.initilizeDeck();
+		testPlayer.printHand();
+		assertEquals(5, testPlayer.getHandSize());
+		testPlayer.playAction();
+		assertEquals(4, testPlayer.getHandSize());
+	}
+	
+	@Test
+	public void testHandRefresh() {
+		Player testPlayer = new Player();
+		testPlayer.initilizeDeck();
+		for(int i = 0; i < 5; ++i) {
+			testPlayer.playAction();
+		}
+		testPlayer.drawCard(1);
+		assertEquals(1, testPlayer.getHandSize());
+	}
 }
 
