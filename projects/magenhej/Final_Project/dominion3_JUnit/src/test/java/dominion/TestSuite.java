66d00c10fba6986912efba272d805f460cc76464
magenhej <magenhej@oregonstate.edu>
2017-03-19 20:02:51 -0700

    Add files via upload

---
+++ projects/magenhej/Final_Project/dominion3_JUnit/src/test/java/dominion/TestSuite.java
@@ -0,0 +1,13 @@
+package dominion;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ AdventurerTest.class, AmbassadorTest.class, BaronTest.class, Council_RoomTest.class, CutpurseTest.class,
+		EmbargoTest.class, FeastTest.class, GardensTest.class, MineTest.class, Sea_HagTest.class,
+		SmithyTest.class })
+public class TestSuite {
+
+}
